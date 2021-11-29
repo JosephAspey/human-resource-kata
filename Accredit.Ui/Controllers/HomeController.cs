1736883df13d7687b34eeeee0f771f86cf77b168
Joseph Aspey <joseph.aspey93@gmail.com>
2021-11-29 22:35:10 +0000

    remove boilder plater code.

--- Accredit.Ui/Controllers/HomeController.cs
+++ Accredit.Ui/Controllers/HomeController.cs
@@ -1,33 +1,12 @@
 ﻿using Accredit.Ui.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
-using System.Threading.Tasks;
-using Accredit.Domain.Repositories;
 
 namespace Accredit.Ui.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IHumanResourceRepository _humanResourceRepository;
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(IHumanResourceRepository humanResourceRepository, ILogger<HomeController> logger)
-        {
-            _humanResourceRepository = humanResourceRepository;
-            _logger = logger;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var resources = await _humanResourceRepository.GetHumanResources();
-
-
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public IActionResult Index()
         {
             return View();
         }
